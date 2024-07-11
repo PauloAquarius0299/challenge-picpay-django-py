50d1dad06056b0e6f5ca36b3ade4847a33b71a26
Paulo Cesar <paulocgsantos99@gmail.com>
2024-07-11 14:39:12 -0300

    feat endpont and users types

--- users/api.py
+++ users/api.py
@@ -1,21 +1,24 @@
 from ninja import Router 
-from .schemas import UserSchema 
+from .schemas import UserSchema, TypeUserSchema 
 from .models import User
 from django.contrib.auth.hashers import make_password
 from django.core.exceptions import ValidationError
+from rolepermissions.roles import assign_role
+
 users_router = Router()
 
 @users_router.post('/', response={200: dict, 400: dict, 500: dict})
-def create_user(request, user: UserSchema):
-    user = User(**user.dict())
-    user.password = make_password(user.password)
+def create_user(request, type_user_schema: TypeUserSchema):
+    print(type_user_schema.user.dict())
+    print(type_user_schema.type_user.dict())
+    user = User(**type_user_schema.user.dict())
+    user.password = make_password(type_user_schema.user.password)
     try:
         user.full_clean()
         user.save()
+        assign_role(user, type_user_schema.type_user.type)
     except ValidationError as e:
         print(e.message_dict)
         return 400, {'errors': e.message_dict}
-    except Exception as e:
-        return 500, {'errors': 'Error interno do servidor'}
     
-    return {'ok': 'ok'}
+    return {'user_id': user.id}
